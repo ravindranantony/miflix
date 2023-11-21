cc8ffce5fcaeedcfe7d53650a8059daebc6cce64
Ravindran Antonysamy <ravindran.antonysamy@centuri.se>
2023-11-21 16:10:42 +0100

    updated

--- mflix/src/app/page.tsx
+++ mflix/src/app/page.tsx
@@ -1,113 +1,353 @@
 import Image from 'next/image'
 
+/**
+ * v0 by Vercel.
+ * @see https://v0.dev/t/fLeLorqvuk4
+ */
+import Link from "next/link"
+import { Button } from "@/components/ui/button"
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="min-h-screen flex flex-col">
+      <header className="flex justify-between items-center bg-black text-white px-6 py-4">
+        <div className="flex items-center gap-2">
+          <span className="font-bold text-xl">MFlix</span>
+          <nav className="flex gap-4">
+            <Link className="hover:underline" href="#">
+              Home
+            </Link>
+            <Link className="hover:underline" href="#">
+              TV Shows
+            </Link>
+            <Link className="hover:underline" href="#">
+              Movies
+            </Link>
+            <Link className="hover:underline" href="#">
+              My List
+            </Link>
+          </nav>
         </div>
-      </div>
+        <div className="flex items-center gap-2">
+          <Link className="text-md hover:underline" href="#">
+            Shop
+          </Link>
+          <span className="text-md">Username</span>
+          <IconUser className="h-6 w-6 text-white" />
+        </div>
+      </header>
+      <main className="flex-1 bg-gray-900 p-6">
+        <div className="grid grid-cols-4 gap-4">
+          <div className="relative group">
+            <img
+              alt="Thumbnail 1"
+              className="rounded-md object-cover transition-opacity group-hover:opacity-50"
+              height="375"
+              src="/placeholder.svg"
+              style={{
+                aspectRatio: "312/375",
+                objectFit: "cover",
+              }}
+              width="312"
+            />
+            <div className="absolute inset-0 flex flex-col justify-center items-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+              <div className="bg-black bg-opacity-50 text-white py-1 px-2 rounded-md text-center">
+                <span>Content Title 1</span>
+              </div>
+              <Button
+                className="border-white text-white hover:bg-white hover:text-black transition-colors duration-300 mt-2"
+                variant="outline"
+              >
+                Play
+              </Button>
+            </div>
+          </div>
+          <div className="relative group">
+            <img
+              alt="Thumbnail 2"
+              className="rounded-md object-cover transition-opacity group-hover:opacity-50"
+              height="375"
+              src="/placeholder.svg"
+              style={{
+                aspectRatio: "312/375",
+                objectFit: "cover",
+              }}
+              width="312"
+            />
+            <div className="absolute inset-0 flex flex-col justify-center items-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+              <div className="bg-black bg-opacity-50 text-white py-1 px-2 rounded-md text-center">
+                <span>Content Title 2</span>
+              </div>
+              <Button
+                className="border-white text-white hover:bg-white hover:text-black transition-colors duration-300 mt-2"
+                variant="outline"
+              >
+                Play
+              </Button>
+            </div>
+          </div>
+          <div className="relative group">
+            <img
+              alt="Thumbnail 3"
+              className="rounded-md object-cover transition-opacity group-hover:opacity-50"
+              height="375"
+              src="/placeholder.svg"
+              style={{
+                aspectRatio: "312/375",
+                objectFit: "cover",
+              }}
+              width="312"
+            />
+            <div className="absolute inset-0 flex flex-col justify-center items-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+              <div className="bg-black bg-opacity-50 text-white py-1 px-2 rounded-md text-center">
+                <span>Content Title 3</span>
+              </div>
+              <Button
+                className="border-white text-white hover:bg-white hover:text-black transition-colors duration-300 mt-2"
+                variant="outline"
+              >
+                Play
+              </Button>
+            </div>
+          </div>
+          <div className="relative group">
+            <img
+              alt="Thumbnail 4"
+              className="rounded-md object-cover transition-opacity group-hover:opacity-50"
+              height="375"
+              src="/placeholder.svg"
+              style={{
+                aspectRatio: "312/375",
+                objectFit: "cover",
+              }}
+              width="312"
+            />
+            <div className="absolute inset-0 flex flex-col justify-center items-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+              <div className="bg-black bg-opacity-50 text-white py-1 px-2 rounded-md text-center">
+                <span>Content Title 4</span>
+              </div>
+              <Button
+                className="border-white text-white hover:bg-white hover:text-black transition-colors duration-300 mt-2"
+                variant="outline"
+              >
+                Play
+              </Button>
+            </div>
+          </div>
+          <div className="relative group">
+            <img
+              alt="Thumbnail 5"
+              className="rounded-md object-cover transition-opacity group-hover:opacity-50"
+              height="375"
+              src="/placeholder.svg"
+              style={{
+                aspectRatio: "312/375",
+                objectFit: "cover",
+              }}
+              width="312"
+            />
+            <div className="absolute inset-0 flex flex-col justify-center items-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+              <div className="bg-black bg-opacity-50 text-white py-1 px-2 rounded-md text-center">
+                <span>Content Title 5</span>
+              </div>
+              <Button
+                className="border-white text-white hover:bg-white hover:text-black transition-colors duration-300 mt-2"
+                variant="outline"
+              >
+                Play
+              </Button>
+            </div>
+          </div>
+          <div className="relative group">
+            <img
+              alt="Thumbnail 6"
+              className="rounded-md object-cover transition-opacity group-hover:opacity-50"
+              height="375"
+              src="/placeholder.svg"
+              style={{
+                aspectRatio: "312/375",
+                objectFit: "cover",
+              }}
+              width="312"
+            />
+            <div className="absolute inset-0 flex flex-col justify-center items-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+              <div className="bg-black bg-opacity-50 text-white py-1 px-2 rounded-md text-center">
+                <span>Content Title 6</span>
+              </div>
+              <Button
+                className="border-white text-white hover:bg-white hover:text-black transition-colors duration-300 mt-2"
+                variant="outline"
+              >
+                Play
+              </Button>
+            </div>
+          </div>
+          <div className="relative group">
+            <img
+              alt="Thumbnail 7"
+              className="rounded-md object-cover transition-opacity group-hover:opacity-50"
+              height="375"
+              src="/placeholder.svg"
+              style={{
+                aspectRatio: "312/375",
+                objectFit: "cover",
+              }}
+              width="312"
+            />
+            <div className="absolute inset-0 flex flex-col justify-center items-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+              <div className="bg-black bg-opacity-50 text-white py-1 px-2 rounded-md text-center">
+                <span>Content Title 7</span>
+              </div>
+              <Button
+                className="border-white text-white hover:bg-white hover:text-black transition-colors duration-300 mt-2"
+                variant="outline"
+              >
+                Play
+              </Button>
+            </div>
+          </div>
+          <div className="relative group">
+            <img
+              alt="Thumbnail 8"
+              className="rounded-md object-cover transition-opacity group-hover:opacity-50"
+              height="375"
+              src="/placeholder.svg"
+              style={{
+                aspectRatio: "312/375",
+                objectFit: "cover",
+              }}
+              width="312"
+            />
+            <div className="absolute inset-0 flex flex-col justify-center items-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+              <div className="bg-black bg-opacity-50 text-white py-1 px-2 rounded-md text-center">
+                <span>Content Title 8</span>
+              </div>
+              <Button
+                className="border-white text-white hover:bg-white hover:text-black transition-colors duration-300 mt-2"
+                variant="outline"
+              >
+                Play
+              </Button>
+            </div>
+          </div>
+          <div className="relative group">
+            <img
+              alt="Thumbnail 9"
+              className="rounded-md object-cover transition-opacity group-hover:opacity-50"
+              height="375"
+              src="/placeholder.svg"
+              style={{
+                aspectRatio: "312/375",
+                objectFit: "cover",
+              }}
+              width="312"
+            />
+            <div className="absolute inset-0 flex flex-col justify-center items-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+              <div className="bg-black bg-opacity-50 text-white py-1 px-2 rounded-md text-center">
+                <span>Content Title 9</span>
+              </div>
+              <Button
+                className="border-white text-white hover:bg-white hover:text-black transition-colors duration-300 mt-2"
+                variant="outline"
+              >
+                Play
+              </Button>
+            </div>
+          </div>
+          <div className="relative group">
+            <img
+              alt="Thumbnail 10"
+              className="rounded-md object-cover transition-opacity group-hover:opacity-50"
+              height="375"
+              src="/placeholder.svg"
+              style={{
+                aspectRatio: "312/375",
+                objectFit: "cover",
+              }}
+              width="312"
+            />
+            <div className="absolute inset-0 flex flex-col justify-center items-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+              <div className="bg-black bg-opacity-50 text-white py-1 px-2 rounded-md text-center">
+                <span>Content Title 10</span>
+              </div>
+              <Button
+                className="border-white text-white hover:bg-white hover:text-black transition-colors duration-300 mt-2"
+                variant="outline"
+              >
+                Play
+              </Button>
+            </div>
+          </div>
+          <div className="relative group">
+            <img
+              alt="Thumbnail 11"
+              className="rounded-md object-cover transition-opacity group-hover:opacity-50"
+              height="375"
+              src="/placeholder.svg"
+              style={{
+                aspectRatio: "312/375",
+                objectFit: "cover",
+              }}
+              width="312"
+            />
+            <div className="absolute inset-0 flex flex-col justify-center items-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+              <div className="bg-black bg-opacity-50 text-white py-1 px-2 rounded-md text-center">
+                <span>Content Title 11</span>
+              </div>
+              <Button
+                className="border-white text-white hover:bg-white hover:text-black transition-colors duration-300 mt-2"
+                variant="outline"
+              >
+                Play
+              </Button>
+            </div>
+          </div>
+          <div className="relative group">
+            <img
+              alt="Thumbnail 12"
+              className="rounded-md object-cover transition-opacity group-hover:opacity-50"
+              height="375"
+              src="/placeholder.svg"
+              style={{
+                aspectRatio: "312/375",
+                objectFit: "cover",
+              }}
+              width="312"
+            />
+            <div className="absolute inset-0 flex flex-col justify-center items-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+              <div className="bg-black bg-opacity-50 text-white py-1 px-2 rounded-md text-center">
+                <span>Content Title 12</span>
+              </div>
+              <Button
+                className="border-white text-white hover:bg-white hover:text-black transition-colors duration-300 mt-2"
+                variant="outline"
+              >
+                Play
+              </Button>
+            </div>
+          </div>
+        </div>
+      </main>
+    </div>
+  )
+}
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-</main>
-)
+function IconUser(props) {
+  return (
+    <svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2" />
+      <circle cx="12" cy="7" r="4" />
+    </svg>
+  )
 }
